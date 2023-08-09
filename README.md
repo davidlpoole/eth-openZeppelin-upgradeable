bcef9b4355346ec2666fe60714569be9cf7b7287
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-08-09 11:53:51 +0100

    hardhat init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Deploy an Upgradeable Vending Machine Smart Contract
+
+Deploying contracts under a proxy can be useful in order to future-proof your smart contract functionality.
+It means being able to deploy a version 1 contract, add new functionality and then deploy a version 2 of that contract
+under the proxy, and so on...
---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_ALCHEMY_API_URL,
+            accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+        }
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY
+    }
+};
